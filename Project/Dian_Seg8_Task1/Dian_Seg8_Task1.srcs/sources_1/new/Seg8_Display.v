b91a056e04dea6a45ff4e7f35a3722528cdb40e2
lxl-git05 <XinLiu051024@outlook.com>
2025-11-17 18:24:42 +0800

    新增电路测试实验课代码-八段数码管显示工程

---
+++ Project/Dian_Seg8_Task1/Dian_Seg8_Task1.srcs/sources_1/new/Seg8_Display.v
@@ -0,0 +1,20 @@
+// 数码管展示字符函数
+module Seg8_Display(
+        clk ,
+        Reset_n ,
+        Display_Data ,
+        Seg_EN ,
+        Seg_LED
+    );
+    // 引脚定义
+    input clk ;
+    input Reset_n ;
+    input [31:0] Display_Data ;
+    output Seg_EN ;
+    output Seg_LED ;
+    // 逻辑书写
+    
+    
+    
+    
+endmodule
